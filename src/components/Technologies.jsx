5f04e1ce93d5b590369922bd4085fbf95f2c8e15
Daniel Vasquez <daniel.vasquez.git@gmail.com>
2024-11-11 00:08:25 -0600

    Adding swapy library

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+import { createSwapy } from "swapy";
 import { TECHNOLOGIES } from "@/constants/technologies";
 import { LogoReact } from "@/components/icons/LogoReact";
 import { LogoAstro } from "@/components/icons/LogoAstro";
@@ -12,6 +14,7 @@ import { LogoGithub } from "@/components/icons/LogoGithub";
 import { LogoRedux } from "@/components/icons/LogoRedux";
 import { LogoWordPress } from "@/components/icons/LogoWordPress";
 import { LogoWoo } from "@/components/icons/LogoWoo";
+import { LogoJetEngine } from "@/components/icons/LogoJetEngine";
 
 const WIDTHLOGO = "w-9";
 const HEIGHTLOGO = "h-9";
@@ -19,88 +22,102 @@ const HEIGHTLOGO = "h-9";
 const LOGOS = [
   {
     name: "astro",
-    logo: <LogoAstro className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-orange-500`} />,
+    logo: <LogoAstro className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-orange-500`} />
   },
   {
     name: "tailwind",
-    logo: <LogoTailwind className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-cyan-300`} />,
+    logo: <LogoTailwind className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-cyan-300`} />
   },
   {
     name: "react",
-    logo: <LogoReact className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-blue-500`} />,
+    logo: <LogoReact className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-blue-500`} />
   },
   {
     name: "nodejs",
-    logo: <LogoNodejs className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-green-500`} />,
+    logo: <LogoNodejs className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-green-500`} />
   },
   {
     name: "express",
-    logo: <LogoNodejs className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-green-500`} />,
+    logo: <LogoNodejs className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-green-500`} />
   },
   {
     name: "javascript",
-    logo: <LogoJavascript className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-yellow-400`} />,
+    logo: <LogoJavascript className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-yellow-400`} />
   },
   {
     name: "python",
-    logo: <LogoPython className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-yellow-400`} />,
+    logo: <LogoPython className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-yellow-400`} />
   },
   {
     name: "shopify",
-    logo: <LogoShopify className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-green-500`} />,
+    logo: <LogoShopify className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-green-500`} />
   },
   {
     name: "aws",
-    logo: <LogoAws className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-orange-400`} />,
+    logo: <LogoAws className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-orange-400`} />
   },
   {
     name: "git",
-    logo: <LogoGit className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-red-500`} />,
+    logo: <LogoGit className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-red-500`} />
   },
   {
     name: "github",
-    logo: <LogoGithub className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-purple-600`} />,
+    logo: <LogoGithub className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-purple-600`} />
   },
   {
     name: "redux",
-    logo: <LogoRedux className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-purple-500`} />,
+    logo: <LogoRedux className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-purple-500`} />
   },
   {
     name: "wordpress",
-    logo: <LogoWordPress className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-blue-400`} />,
+    logo: <LogoWordPress className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-blue-400`} />
   },
   {
     name: "woo",
-    logo: <LogoWoo className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-purple-400`} />,
+    logo: <LogoWoo className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-purple-400`} />
+  },
+  {
+    name: "jetengine",
+    logo: <LogoJetEngine className={`${WIDTHLOGO} ${HEIGHTLOGO} m-auto text-purple-400`} />
   },
 ];
 
 export const Technologies = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const swapyInstance = createSwapy(containerRef.current);
+      return () => swapyInstance.disable();
+    }
+  }, []);
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div
+      ref={containerRef}
+      className="grid justify-items-center grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5"
+    >
       {TECHNOLOGIES.map(({ name, link, nameLogo }, index) => (
         <div
           key={index}
-          className={`w-48 m-auto border border-border shadow-lg flex flex-col justify-center cursor-pointer p-3 rounded-lg trasition duration-300 ease-in-out transform ${link ? "text-blue-600 hover:underline" : "text-gray-600"} ${link ? "hover:text-golden" : "hover:text-white"} hover:bg-blue-medium hover:-translate-y-2 md:w-auto md:m-0`}
+          className="swapy-item"
+          data-swapy-slot={`slot-${index}`}
         >
-          <a
-            className="flex items-center gap-3"
-            href={link}
-            target="_blank"
+          <a 
+            className="w-40 h-28 border border-border shadow-lg flex flex-col justify-center items-center gap-3 cursor-grabbing rounded-lg text-gray-600 hover:text-white hover:bg-blue-medium md:m-0" 
+            href={link} 
+            target="_blank" 
+            data-swapy-item={`item-${index}`}
           >
-            {LOGOS.map(({ name, logo }, index) => (
-              nameLogo === name && (
-                <div key={index}>
-                  {logo}
-                </div>
-              )
-            ))}
-            <p className={`text-md text-center font-bold tracking-tight`}>
+            {LOGOS.map(({ name, logo }, idx) => nameLogo === name && 
+              <div key={idx}>{logo}</div>
+            )}
+            <p className="text-sm text-center font-bold tracking-tight">
               {name}
             </p>
           </a>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
